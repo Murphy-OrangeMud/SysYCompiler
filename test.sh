5f61417c8d4d530fdfa83bda30342d250962a83d
Murphy-OrangeMud <chengzhiyi_2@163.com>
2021-04-26 12:53:30 +0800

    test script

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+filepath="../open-test-cases/eeyore/functional"
+
+cd cmake-build-debug || exit
+
+for (( i = 0; i < 100; i++ ))
+do
+  i_path=$filepath/$(printf "%02d_*.sy" $i)
+  o_path=$filepath/$(printf "%02d.eeyore" $i)
+  ./sysyc "$i_path" "$o_path"
+  # printf "%s %s\n" "$i_path" "$o_path"
+done
