0a7a4b851a5849bbf8bac311442cea590a4b4072
Murphy-OrangeMud <chengzhiyi_2@163.com>
2021-04-28 23:00:49 +0800

    definition of var and func info

--- src/mid/typeck.hpp
+++ src/mid/typeck.hpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <set>
 #include <string>
+#include <utility>
 #include <vector>
 #include "../define/token.hpp"
 #include "../front/logger.hpp"
@@ -12,80 +13,66 @@
 class TypeCheck {
 private:
     Logger logger;
-    std::string currentFunc;
+    int blockNum;
 public:
-    std::map<std::string, int> VarTable;
-    std::map<std::string, int> ConstVarTable;
-    std::map <std::string, std::vector<int>> ArrayTable;
-    std::map <std::string, std::vector<int>> ConstArrayTable;
+    struct Var {
+        std::string name;
+        VarType argType;
+        std::vector<int> dims;
+        bool isConst;
+        int val;
 
-    std::map <std::string, std::pair<Type,
-            std::vector < std::pair < std::string, std::pair < VarType, std::vector < int>/*dims*/>>>/* Arg table */>>
-    FuncTable;
-    std::map <std::string, std::set<std::string>> FuncVarTable;
-    std::map <std::string, std::map<std::string, int>> FuncConstVarTable;
-    std::map <std::string, std::map<std::string, std::vector < int>>>
-    FuncArrayTable;
-    std::map <std::string, std::map<std::string, std::vector < int>>>
-    FuncConstArrayTable;
+        Var(std::string _n, VarType _t, bool _c, std::vector<int> _d = std::vector<int>{}, int _v = 0) : name(
+                std::move(_n)), argType(_t), isConst(_c), dims(std::move(_d)), val(_v) {}
+    };
 
-    TypeCheck(const std::string& i) {
+    struct Function {
+        std::string funcName;
+        Type funcType;
+        std::vector<Var> argTable;
+
+        Function(std::string _n, Type _t, std::vector<Var> _a) : funcName(std::move(_n)), funcType(_t),
+                                                                 argTable(std::move(_a)) {}
+    };
+
+    std::vector<int> parentBlock;
+
+    std::map<std::string, Function> FuncTable;
+    std::map<int, Var> BlockVars;
+
+    TypeCheck(const std::string &i) {
         std::string path = R"(../../logs/log_checker_)" + i;
         logger = Logger(path);
-        currentFunc = "";
-        // insert 6 included func
-        FuncTable["getint"] = std::make_pair(Type::INT, std::vector < std::pair < std::string, std::pair < VarType,
-                                             std::vector < int >/*dims*/>>>{});
-        FuncTable["getch"] = std::make_pair(Type::INT, std::vector < std::pair < std::string, std::pair < VarType,
-                                            std::vector < int >/*dims*/>>>{});
-        FuncTable["getarray"] = std::make_pair(Type::INT, std::vector < std::pair < std::string, std::pair < VarType,
-                                               std::vector < int >/*dims*/>>>{
-            std::make_pair("a", std::make_pair(
-                    VarType::ARRAY, std::vector < int > {0}))
-        });
-        FuncTable["putint"] = std::make_pair(Type::VOID, std::vector < std::pair < std::string, std::pair < VarType,
-                                             std::vector < int >/*dims*/>>>{
-            std::make_pair("a",
-                           std::make_pair(VarType::VAR,
-                                          std::vector <
-                                          int > {}))
-        });
-        FuncTable["putch"] = std::make_pair(Type::VOID, std::vector < std::pair < std::string, std::pair < VarType,
-                                            std::vector < int >/*dims*/>>>{
-            std::make_pair("a",
-                           std::make_pair(VarType::VAR,
-                                          std::vector <
-                                          int > {}))
-        });
-        FuncTable["putarray"] = std::make_pair(Type::VOID, std::vector < std::pair < std::string, std::pair < VarType,
-                                               std::vector < int >/*dims*/>>>{
-            std::make_pair("a", std::make_pair(
-                    VarType::VAR, std::vector < int > {})), std::make_pair("b", std::make_pair(VarType::ARRAY,
-                                                                                               std::vector < int > {0}))
-        });
+
+        FuncTable["getint"] = Function("getint", Type::INT, std::vector<Var>{});
+        FuncTable["getch"] = Function("getch", Type::INT, std::vector<Var>{});
+        FuncTable["getaarray"] = Function("getarray", Type::INT, std::vector<Var>{Var("a", VarType::ARRAY, false, std::vector<int>{0})});
+        FuncTable["putint"] = Function("putint", Type::VOID, std::vector<Var>{Var("a", VarType::VAR, false)});
+        FuncTable["putch"] = Function("putch", Type::VOID, std::vector<Var>{Var("a", VarType::VAR, false)});
+        FuncTable["putarray"] = Function("putarray", Type::VOID, std::vector<Var>{Var("a", VarType::VAR, false), Var("b", VarType::ARRAY, false, std::vector<int>{0})});
     }
 
     ~TypeCheck() = default;
 
     bool FillInValue(int *memory, InitValAST *init, std::vector<int> &dim, size_t i);
 
-    std::unique_ptr <VarDeclAST> EvalVarDecl(VarDeclAST &varDecl);
+    std::unique_ptr<VarDeclAST> EvalVarDecl(VarDeclAST &varDecl);
 
-    std::unique_ptr <ProcessedIdAST> EvalId(IdAST &id);
+    std::unique_ptr<ProcessedIdAST> EvalId(IdAST &id);
 
-    std::unique_ptr <VarDefAST> EvalVarDef(VarDefAST &varDef);
+    std::unique_ptr<VarDefAST> EvalVarDef(VarDefAST &varDef);
 
-    std::unique_ptr <FuncCallAST> EvalFuncCall(FuncCallAST &func);
+    std::unique_ptr<FuncCallAST> EvalFuncCall(FuncCallAST &func);
 
-    std::unique_ptr <BlockAST> EvalBlock(BlockAST &block);
+    std::unique_ptr<BlockAST> EvalBlock(BlockAST &block);
 
-    std::unique_ptr <IfElseAST> EvalIfElse(IfElseAST &stmt);
+    std::unique_ptr<IfElseAST> EvalIfElse(IfElseAST &stmt);
 
-    std::unique_ptr <WhileAST> EvalWhile(WhileAST &stmt);
+    std::unique_ptr<WhileAST> EvalWhile(WhileAST &stmt);
 
-    std::unique_ptr <ControlAST> EvalControl(ControlAST &stmt);
+    std::unique_ptr<ControlAST> EvalControl(ControlAST &stmt);
 
-    std::unique_ptr <AssignAST> EvalAssign(AssignAST &assign);
+    std::unique_ptr<AssignAST> EvalAssign(AssignAST &assign);
 
     ASTPtr EvalLVal(LValAST &lval);
 
@@ -95,7 +82,7 @@ public:
 
     ASTPtr EvalUnaryExp(UnaryExpAST &exp);
 
-    std::unique_ptr <FuncDefAST> EvalFuncDef(FuncDefAST &funcDef);
+    std::unique_ptr<FuncDefAST> EvalFuncDef(FuncDefAST &funcDef);
 
     ASTPtr EvalRelExp(BinaryExpAST &exp);
 
@@ -103,13 +90,13 @@ public:
 
     ASTPtr EvalLOrExp(BinaryExpAST &exp);
 
-    std::unique_ptr <CompUnitAST> EvalCompUnit(CompUnitAST &unit);
+    std::unique_ptr<CompUnitAST> EvalCompUnit(CompUnitAST &unit);
 
     ASTPtr EvalEqExp(BinaryExpAST &exp);
 
-    std::unique_ptr <StmtAST> EvalStmt(StmtAST &stmt);
+    std::unique_ptr<StmtAST> EvalStmt(StmtAST &stmt);
 
-    std::unique_ptr <InitValAST> EvalInitVal(InitValAST &init);
+    std::unique_ptr<InitValAST> EvalInitVal(InitValAST &init);
 };
 
 #endif
